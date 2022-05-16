229a4aa9f36641c99d5d63021a2b438ad9ed63b2
Ajor Virtuous <96791819+Smilez21@users.noreply.github.com>
2022-05-16 15:28:18 +0000

    Add files via upload

---
+++ main.blade.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>\</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- link to css -->
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+  </head>
+  <body> 
+    <!-- hero section start -->
+    <div class="hero">
+    <nav>
+      <h2 class="logo">Smilez<span>Homes</span>.</h2>
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Sales">Sales</a></li>
+        <li><a href="Properties">Properties</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+          <a href="/login" class="btn">Login</a>
+    </nav>
+
+    <div class="smilez">
+    <h1>Find Your Next<br>Perfect Place To <br>Live</h1>
+      <div class="motto">
+      <a href="#" class="btn">Signup</a>
+      
+</div>
+</div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<!--About-->
+<section class="about container" id="about">
+        <div class="about-img">
+            <img src= "images/homepage1.jpeg"  alt="">
+        </div>
+        <div class="about-text">
+            <span>About Us</span>
+            <h2>We Provide The Best <br>Property For You !</h2>
+            <p>JosCheck is a real estate firm that specializes in investment,development and managment.</p>
+            <p>We provide solution in the procurement and provision of premium land and housing investment oppurtunities in jos.</p>
+            <a href="#" class="btn">Learn More</a>
+        </div>
+        <br>
+        </div>
+    </section>
+    </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <!--Sales-->
+      <section class="sales container" id="sales">
+        <!--Box 1-->
+        <div class="box">
+            <i class='bx bx-user' ></i>
+            <h3>Make Your Dream True</h3>
+            <p>pick any and enjoy the comfort.</p>
+        </div>
+        <!--Box 2-->
+        <div class="box">
+            <i class='bx bx-desktop' ></i>
+            <h3>Start Your Membership</h3>
+            <p>Sign up at the Home page</p>
+        </div>
+        <!--Box 1-->
+        <div class="box">
+            <i class='bx bx-home-alt' ></i>
+            <h3>Enjoy Your New Home</h3>
+            <p>Luxury lifestyle</p>
+        </div>
+    </section>
+       <!--Properties-->
+    <section class="properties container" id="properties">
+        <div class="heading">
+            <span>Recent</span>
+            <h2>Our Featured properties</h2>
+        </div><br>
+        <br>
+        <div class="properties-container container">
+            <!--box 1-->
+            <div class="box">
+                
+                <img src= "images/bn13.jpg" alt="">
+                <h3>N25,000,000</h3>
+                <div class="content">
+                    <div class="text">
+                    <h3>Dadin kowa</h3>
+                    <p>Jos. Plateau</p>
+                </div>
+                <div class="icon">
+                    <i class='bx bx-bed' ><span>5</span></i>
+                    <i class='bx bx-bath' ><span>2</span></i>
+                    <a href="signup.html">Book</a>
+                </div>
+            </div>
+        </div>
+           <!--box 2-->
+           <div class="box">
+            <img src= "images/bn11.jpg"  alt="">
+            <h3>N32,000,000</h3>
+            <div class="content">
+                <div class="text">
+                <h3>Atiku Street, Rayfield</h3>
+                <p> Bungalow Jos,Plateau</p>
+            </div>
+            <div class="icon">
+                <i class='bx bx-bed' ><span>5</span></i>
+                <i class='bx bx-bath' ><span>2</span></i>
+                <a href="signup.html">Book</a>
+            </div>
+        </div>
+    </div>
+       <!--box 3-->
+       <div class="box">
+        <img src="images/bn12.jpg" alt="">
+        <h3>N30,000,000</h3>
+        <div class="content">
+            <div class="text">
+            <h3>Hill Station</h3>
+            <p>Bungalow Jos,Plateau</p>
+        </div>
+        <div class="icon">
+            <i class='bx bx-bed' ><span>5</span></i>
+            <i class='bx bx-bath' ><span>2</span></i>
+            <a href="signup.html">Book</a>
+        </div>
+    </div>
+</div>
+   <!--box 4-->
+   <div class="box">
+    <img src="images/bn6.jpg" alt="">
+    <h3>N40,000,000</h3>
+    <div class="content">
+        <div class="text">
+        <h3>Fwavei,Rayfield</h3>
+        <p>Jos. Plateau</p>
+    </div>
+    <div class="icon">
+        <i class='bx bx-bed' ><span>5</span></i>
+        <i class='bx bx-bath' ><span>2</span></i>
+        <a href="signup.html">Book</a>
+    </div>
+</div>
+</div>
+   <!--box 5-->
+   <div class="box">
+    <img src="images/bn9.jpg" alt="">
+    <h3>N38,000,000</h3>
+    <div class="content">
+        <div class="text">
+        <h3>Zaramanganda</h3>
+        <p> 3 bedroom Bungalow Jos,Plateau</p>
+    </div>
+    <div class="icon">
+        <i class='bx bx-bed' ><span>5</span></i>
+        <i class='bx bx-bath' ><span>2</span></i>
+        <a href="signup.html">Book</a>
+    </div>
+</div>
+</div>
+   <!--box 6-->
+   <div class="box">
+    <img src="images/bn15.jpg" alt="">
+    <h3>N43,000,000</h3>
+    <div class="content">
+        <div class="text">
+        <h3>Airforce</h3>
+        <p> 2 bedroom Duplex with basement,Jos Plateau</p>
+    </div>
+    <div class="icon">
+        <i class='bx bx-bed' ><span>5</span></i>
+        <i class='bx bx-bath' ><span>2</span></i>
+        <a href="signup.html">Book</a>
+    </div>
+</div>
+</div>
+    </section>
+    <!--News Letter-->
+    <section class="newsletter container">
+        <h2>Have Question in mind?<br>Let us help you</h2>
+        <form action="">
+            <input type="email" name="" id="email-box" placeholder="yourmail@gmail.com" required>
+            <input type="submit" value="Send" class="btn">
+        </form>
+    </section>
+    <!--Footer-->
+    <div class="footer1">
+    <section class="footer">
+        <div class="footer-container container">
+            <h2>JosCheck</h2>
+            <div class="footer-box">
+                <h3>Quick Links</h3>
+                <a href="#">Agency</a>
+                <a href="#">Buildings</a>
+                <a href="#">Rates</a>
+            </div>
+            <div class="footer-box">
+                <h3>Locations</h3>
+                <a href="#">Jos-south</a>
+                <a href="#">Jos-north</a>
+                <a href="#">Jos-east</a>
+            </div>
+            <div class="footer-box">
+                <h3>Contact</h3>
+                <a href="#">+234 8063512382</a>
+                <a href="#">Joscheck@gmail.com</a>
+                <div class="social"></div>
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+            </div>
+
+        </div>
+    </section>
+    <!--copyright-->
+    <div class="copyright">
+        <p>&#169; JosCheck All Right Reserved</p>
+    </div>
+
+   
+        
+            
+        
+    </div>
+</section>
+</body>
+</html>
+
